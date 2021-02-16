06ffa7ba71c435dbff917e0cbaaa0259ee765b6e
Pascal Perrot <precisiondevpy@gmail.com>
2021-02-16 18:48:42 +0100

    display landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 const Landing = () => {
-    return (
-        <main className='welcomePage'>
 
+    const [btn, setBtn] = useState(false);
+
+    const refWolverine = useRef(null);
+    console.log(btn);
+
+    useEffect(() => {
+        refWolverine.current.classList.add('startingImg');
+        setTimeout(() => {
+            refWolverine.current.classList.remove('startingImg');
+            setBtn(true);
+        }, 1000)
+    }, [])
+
+    // display griffes
+    const setLeftImg = () => {
+        refWolverine.current.classList.add('leftImg');
+    }
+    const setRightImg = () => {
+        refWolverine.current.classList.add('rightImg');
+    }
+    const clearImg = () => {
+        if (refWolverine.current.classList.contains('leftImg')) {
+            refWolverine.current.classList.remove('leftImg');
+        } else {
+            refWolverine.current.classList.remove('rightImg');
+        }
+    }
+
+    const displayBtn = btn && (                  //if (btn) { return ( ... )}
+        <>
+            <div onMouseOver={ setLeftImg } onMouseOut={ clearImg } className="leftBox">
+                <button className="btn-welcome">Inscription</button>
+            </div>
+            <div onMouseOver={ setRightImg } onMouseOut={ clearImg } className="rightBox">
+                <button className="btn-welcome">Connexion</button>
+            </div>
+        </>
+    )
+
+    return (
+        <main ref={ refWolverine } className='welcomePage'>
+            { displayBtn }
         </main>
     );
 };
