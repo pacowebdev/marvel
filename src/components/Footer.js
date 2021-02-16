293b24e427b6ce9e10b670dbabe629d7b4ff7221
Pascal Perrot <precisiondevpy@gmail.com>
2021-02-16 16:46:56 +0100

    Initial templates

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='footer-container'>
+                <p>Projet réalisé par Pacowebdev - 2021</p>
+            </div>
+
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='banner-container'>
+            <h1><a href="/">Marvel Quiz</a></h1>
+        </div>
+    );
+};
+
+export default Header;
